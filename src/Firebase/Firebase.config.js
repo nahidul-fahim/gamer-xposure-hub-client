52d8fae09ff48b50b9494d8e18d1097a90990fb5
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-10-07 22:53:06 +0600

    created necessary pages and routes, installed firebase and imported firebase SDK

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIuDwmRyTt5ubIMi-d0Y52wC4fPs5WiFo",
+  authDomain: "gamer-xposure-hub.firebaseapp.com",
+  projectId: "gamer-xposure-hub",
+  storageBucket: "gamer-xposure-hub.appspot.com",
+  messagingSenderId: "743941092878",
+  appId: "1:743941092878:web:0867d1f0f48d9cf33c2e5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#00296b',
+      'secondary': '#fdc500',
+    },
+    extend: {
+      fontFamily: {
+        primary: "'Asap', sans-serif",
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
